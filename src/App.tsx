b613c0367de71d7b088b5e53fa7ead4fbb8f58b8
xowoony <trumanblack@hanmail.net>
2023-04-03 17:05:45 +0900

    Router 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import styled from "styled-components";
-
-interface DummyProps {
-  text: string;
-}
-
-// Container
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-// H1
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-// Dummy
-function Dummy({ text }: DummyProps) {
-  return <H1>{text}</H1>;
-}
+import Router from "./Router";
 
 function App() {
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
-
-  return (
-    <Container>
-      <Dummy text="dd" />
-      <button onClick={onClick}>Click me</button>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+// Switch 대신 Routes를 써주도록 한다. (버전관련 이슈)
+// Routes 자식으로는 Route 밖에 올 수 없다.
+// Route 자식으로 뭔가 올 수 없기 때문에 element로 추가해주도록 한다.
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* home - 코인들 */}
+        <Route path="/" element={<Coins />} />
+        {/* Coin ID - 해당 코인정보 */}
+        <Route path="/:coinId" element={<Coin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default Router;
+// 작성해주고 App.tsx로 가서 Router를 렌더링해준다.
