6bec49ca9963343c035d6aa530ae08048c7c9a11
xowoony <trumanblack@hanmail.net>
2023-04-04 19:40:28 +0900

    Coin array 생성

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,63 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li``;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+// Coins array
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  return <h1>Coins</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {/* 코인은 symbol, name 등등이 있다. */}
+      {/* coin.name, coin.symbol 로 작성하여 불러올 수 있다 */}
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>{coin.name}</Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
