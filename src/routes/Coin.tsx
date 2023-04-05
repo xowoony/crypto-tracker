859d9d2b4d22d8aeaab83b7dac88e4231fdbc609
xowoony <trumanblack@hanmail.net>
2023-04-05 18:19:36 +0900

    각 코인 페이지에서 코인 이름 불러오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,53 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// 여기는 각각의 코인 페이지
+
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 700px;
+  margin: 0 auto;
+  // 화면을 크게 했을 때에도 모바일 화면처럼 가운데에 위치하게 됨
+`;
+
+const Header = styled.header`
+  height: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  font-size: 38px;
+  display: block;
+`;
+
+
 
 function Coin() {
   // react-router-dom v6 이상의 경우
   // useParams쓰는 순간 타입이 string or undefined로 설정됨.
   // 따라서 const {coinId} = useParams(); 로만 적어줘도 상관 ㄴ
-  const { coinId } = useParams();
+  const { coinId } = useParams(); // coinId를 받아서 parameter로 사용
+  const [loading, setLoading] = useState(true);
 
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{coinId}</Title>
+      </Header>
+      {loading ? <Loader>로딩중입니다...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
+
