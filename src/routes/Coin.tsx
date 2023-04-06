7a7fb278c38d064d490f5feb0eb16f135ae592f1
xowoony <trumanblack@hanmail.net>
2023-04-06 20:03:32 +0900

    필요한 object key, value 작성

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -54,9 +54,6 @@ interface InfoData {
   is_active: boolean;
   type: string;
   logo: string;
-  tags: []; // type이 object로 뜸. 이 경우 ITag라는 인터페이스를 만듦
-  // []; 라고 작성 후 위에서 인터페이스를 만들어 주어야 함.
-  team: object;
   description: string;
   message: string;
   open_source: boolean;
@@ -66,14 +63,43 @@ interface InfoData {
   proof_type: string;
   org_structure: string;
   hash_algorithm: string;
-  links: object;
-  links_extended: object;
-  whitepaper: object;
   first_data_at: string;
   last_data_at: string;
 }
 
-interface PriceData {}
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
 
 function Coin() {
   // react-router-dom v6 이상의 경우
@@ -85,6 +111,7 @@ function Coin() {
   const { state } = useLocation() as RouteState;
 
   // info state
+  // useState<InfoData>({}); 를 작성해줌으로써 타입스크립트는 info가 InfoData라고 인식
   const [info, setInfo] = useState({});
 
   // priceInfo state
