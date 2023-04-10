ba5b5dc09399d40e57768d9c1f25a404955ff240
xowoony <trumanblack@hanmail.net>
2023-04-10 13:20:43 +0900

    react query 설치

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// queryClient 작성
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <ThemeProvider theme={darkTheme}>
-    <App />
-  </ThemeProvider>
+  // provider 를 만든다 - ThemeProvider, App 컴포넌트를 잘라내고 QueryClientProvider 안에 넣는다.
+  <QueryClientProvider client={queryClient}>
+    {" "}
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
